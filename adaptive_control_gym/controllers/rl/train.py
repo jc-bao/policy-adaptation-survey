61f4de4fc8182c1dd4dc29a9b3375b1fcfe053dc
Reed Pan <reedpan@chaoyi.local>
2022-12-12 08:19:38 +0800

    add logger

--- adaptive_control_gym/controllers/rl/train.py
+++ adaptive_control_gym/controllers/rl/train.py
@@ -1,5 +1,6 @@
 import torch
 import wandb
+from tqdm import trange
 
 from adaptive_control_gym.envs import HoverEnv  
 from adaptive_control_gym.controllers import PPO
@@ -17,15 +18,30 @@ def train():
     agent = PPO(net_dims, env.state_dim, env.action_dim, env_num, gpu_id)
 
     if use_wandb:
-        wandb.init(project="adaptive-control-gym", entity="jason-zhang")
-        wandb.watch(agent.actor)
-        wandb.watch(agent.critic)
+        wandb.init(project="test-project", entity="adaptive-control") 
 
-    for i_ep in range(total_steps//env.max_steps//env_num):
-        states, actions, logprobs, rewards, undones = agent.explore_env(env, env.max_steps)
-        torch.set_grad_enabled(True)
-        logging_tuple = agent.update_net(states, actions, logprobs, rewards, undones)
-        torch.set_grad_enabled(False)
-        if i_ep % eval_freq == 0:
-            states, actions, logprobs, rewards, undones = agent.explore_env(env, env.max_steps, deterministic=True)
-            print(f"Episode {i_ep} | {logging_tuple}")
+    steps_per_ep = env.max_steps*env_num
+    with trange(total_steps//steps_per_ep) as t:
+        for i_ep in t:
+            # train
+            total_steps = i_ep * steps_per_ep
+            states, actions, logprobs, rewards, undones = agent.explore_env(env, env.max_steps)
+            torch.set_grad_enabled(True)
+            critic_loss, actor_loss, action_std = agent.update_net(states, actions, logprobs, rewards, undones)
+            torch.set_grad_enabled(False)
+            if use_wandb:
+                wandb.log({
+                    'train/rewards': rewards.mean().item(), 
+                    'train/actor_loss': actor_loss, 
+                    'train/critic_loss': critic_loss,
+                    'train/action_std': action_std 
+                }, step=total_steps)
+            t.set_postfix(actor_loss=actor_loss, critic_loss=critic_loss, rewards=rewards, steps = total_steps)
+
+            # evaluate
+            if i_ep % eval_freq == 0:
+                states, actions, logprobs, rewards, undones = agent.explore_env(env, env.max_steps, deterministic=True)
+                if use_wandb:
+                    wandb.log({
+                        'eval/rewards': rewards.mean().item(), 
+                    }, step=total_steps)
