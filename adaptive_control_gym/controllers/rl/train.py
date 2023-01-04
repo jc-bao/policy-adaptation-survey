cb6a5875d077efb2a0653b176edc454a46c3f1b5
jc-bao <jc-bao@outlook.com>
2023-01-04 15:08:14 +0800

    update enviroment parameters

--- adaptive_control_gym/controllers/rl/train.py
+++ adaptive_control_gym/controllers/rl/train.py
@@ -24,8 +24,8 @@ class Args:
 
 def train(args:Args)->None:
     env_num = 1024
-    total_steps = 8e6
-    adapt_steps = 0e6
+    total_steps = 4e6
+    adapt_steps = 4e6
     eval_freq = 4
     curri_thereshold = 0.0
     
--- adaptive_control_gym/envs/drone.py
+++ adaptive_control_gym/envs/drone.py
@@ -33,11 +33,11 @@ class DroneEnv(gym.Env):
         self.curri_param = 1.0
         self.adapt_horizon = 3
 
-        self.mass_min, self.mass_max = 0.01, 0.05
+        self.mass_min, self.mass_max = 0.01, 0.05 #0.01, 0.05
         self.delay_min, self.delay_max = 0, 0
-        self.decay_min, self.decay_max = 0.0, 0.3
+        self.decay_min, self.decay_max = 0.15, 0.15 #0.0, 0.3
         self.res_dyn_param_min, self.res_dyn_param_max = -1.0, 1.0
-        self.disturb_min, self.disturb_max = -0.8, 0.8
+        self.disturb_min, self.disturb_max = 0.0, 0.0 #-0.8, 0.8
         self.action_noise_std, self.obs_noise_std = 0.00, 0.00
 
         # generated parameters
