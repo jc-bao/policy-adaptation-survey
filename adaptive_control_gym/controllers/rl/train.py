b5baba0d0ac0e854799f17f336cd277c1e79ff66
jc-bao <jc-bao@outlook.com>
2023-02-01 22:18:07 +0800

    add compresor visualizer

--- adaptive_control_gym/controllers/rl/train.py
+++ adaptive_control_gym/controllers/rl/train.py
@@ -232,7 +232,7 @@ def eval_env(env:DroneEnv, agent:PPO, deterministic:bool=True, use_adaptor:bool=
     # env.mass_max = 0.006+0.024*0.7
     # env.decay_max = 0.1*0.7
     # env.disturb_max = -0.8+1.6*0.7
-    # env.res_dyn_param_max = -1+2.0*0.5
+    # env.res_dyn_param_max = -1+2.0*0.7
     # env.res_dyn_scale = 0.0
     # env.res_dyn = env.res_dyn_fit
     # assert(env.res_dyn_fit!=env.res_dyn_origin)
