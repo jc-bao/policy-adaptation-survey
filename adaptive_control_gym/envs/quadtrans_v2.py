:bug: resolve drone rotation error calculation error

adaptive_control_gym/envs/quadtrans_v2.py
@@ -42,7 +42,7 @@ class QuadTransEnv(gym.Env):
         self.mass_obj = torch.ones((self.env_num, 3), device=self.device) * 0.01
         self.rope_length = torch.ones((self.env_num, self.drone_num), device=self.device) * 0.2
         self.rope_zeta = torch.ones((self.env_num, self.drone_num), device=self.device) * 0.7
-        self.rope_wn = torch.ones((self.env_num, self.drone_num), device=self.device) * 100.0
+        self.rope_wn = torch.ones((self.env_num, self.drone_num), device=self.device) * 1000.0
 
         # state variables
         self.xyz_drones = torch.zeros((self.env_num, self.drone_num, 3), device=self.device)
@@ -157,8 +157,14 @@ class Logger:
         # plot
         x_time = np.arange(len(self.log_dict[self.log_items[0]])) * 4e-4
         for i, item in enumerate(self.log_items):
-            axs[i].plot(x_time, self.log_dict[item])
+            if len(self.log_dict[item][0]) > 1:
+                # plot each dimension
+                for j in range(len(self.log_dict[item][0])):
+                    axs[i].plot(x_time, np.array(self.log_dict[item])[:, j], label=f'{item}_{j}')
+            else:
+                axs[i].plot(x_time, self.log_dict[item])
             axs[i].set_title(item)
+            axs[i].legend()
         # save
         fig.savefig(filename)
 
@@ -166,8 +172,10 @@ def main():
     env = QuadTransEnv(env_num=1, drone_num=1, gpu_id=-1)
     env.reset()
     logger = Logger()
-    for i in range(1000):
-        state = env.simstep(torch.zeros((1, 3)), torch.zeros((1, 1)))
+    for i in range(100):
+        torque = torch.tensor([[0.00, 0.0, 0.0]], dtype=torch.float32)
+        thrust = torch.ones((1, 1))*0.037*9.81
+        state = env.simstep(torque, thrust)
         logger.log(state)
     logger.plot('results/test.png')
     env.close()

adaptive_control_gym/utils/geom.py
@@ -78,17 +78,17 @@ def quat_mul(q1: torch.Tensor, q2: torch.Tensor) -> torch.Tensor:
 
 def integrate_quat(quat, omega, dt):
     # integrate quaternion with angular velocity
-    # quat: (batch_size, 4) with w, x, y, z
+    # quat: (batch_size, 4) with x, y, z, w
     # omega: (batch_size, 3) with x, y, z
     # dt: (batch_size, 1)
-    # quat: (batch_size, 4) with w, x, y, z
+    # quat: (batch_size, 4) with x, y, z, w
 
     bs = quat.shape[:-1]
-    w, x, y, z = quat[..., 0], quat[..., 1], quat[..., 2], quat[..., 3]
+    x, y, z, w = quat[..., 0], quat[..., 1], quat[..., 2], quat[..., 3]
     wx, wy, wz = omega[..., 0], omega[..., 1], omega[..., 2]
     q = torch.zeros([*bs, 4], device=quat.device)
-    q[..., 0] = w + 0.5 * dt * (-wx * x - wy * y - wz * z)
-    q[..., 1] = x + 0.5 * dt * (wx * w + wy * z - wz * y)
-    q[..., 2] = y + 0.5 * dt * (-wx * z + wy * w + wz * x)
-    q[..., 3] = z + 0.5 * dt * (wx * y - wy * x + wz * w)
+    q[..., 0] = x + 0.5 * dt * (w * wx + y * wz - z * wy)
+    q[..., 1] = y + 0.5 * dt * (w * wy - x * wz + z * wx)
+    q[..., 2] = z + 0.5 * dt * (w * wz + x * wy - y * wx)
+    q[..., 3] = w + 0.5 * dt * (-x * wx - y * wy - z * wz)
     return q