7af9e5bf770ba3d2bf967993ead57e1441acd39b
jc-bao <jc-bao@outlook.com>
2023-05-23 15:25:49 +0800

    :zap: add slurm support

--- adaptive_control_gym/controllers/rl/script/srun.sh
+++ adaptive_control_gym/controllers/rl/script/srun.sh
@@ -1,2 +1,2 @@
 wandb offline
-srun -n 1 -N 1 -p gpu python train.py --exp-name "TrackRMA"  --task track --act-expert-mode 1 --cri-expert-mode 1 --use-wandb --curri-param-init 0.3
+srun -n 1 -N 1 -p gpu python train.py --exp-name "TrackRMA"  --task track --act-expert-mode 1 --cri-expert-mode 1 --use-wandb --curri-param-init 0.3 --use-wandb
