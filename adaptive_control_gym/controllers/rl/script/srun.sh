17d4e2eb4b3a147d087da7f28c216e33673b5977
jc-bao <jc-bao@outlook.com>
2023-05-23 16:32:35 +0800

    :zap: add disable object to debug environment

--- adaptive_control_gym/controllers/rl/script/srun.sh
+++ adaptive_control_gym/controllers/rl/script/srun.sh
@@ -1,2 +1,4 @@
 wandb offline
-srun -n 1 -N 1 -p gpu python train.py --exp-name "TrackRMA"  --task track --act-expert-mode 1 --cri-expert-mode 1 --use-wandb --curri-param-init 0.3
+srun -n 1 -N 1 -p gpu python train.py --exp-name "DroneRMA" --act-expert-mode 1 --cri-expert-mode 1 --use-wandb --gpu-id 2
+
+# srun -n 1 -N 1 -p gpu python train.py --exp-name "DroneRobust" --act-expert-mode 0 --cri-expert-mode 0 --use-wandb --gpu-id 3
