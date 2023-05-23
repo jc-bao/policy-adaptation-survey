bfaa4b44e3f4a3a72e7fd17cd5b2de243a8ba37a
jc-bao <jc-bao@outlook.com>
2023-05-23 16:45:29 +0800

    :zap: add data plotter

--- adaptive_control_gym/controllers/rl/script/srun.sh
+++ adaptive_control_gym/controllers/rl/script/srun.sh
@@ -1,4 +1,4 @@
 wandb offline
-srun -n 1 -N 1 -p gpu python train.py --exp-name "DroneRMA" --act-expert-mode 1 --cri-expert-mode 1 --use-wandb --gpu-id 2
+srun -n 1 -N 1 -p gpu -w gnode01 python train.py --exp-name "DroneEnv1024RMA" --act-expert-mode 1 --cri-expert-mode 1 --use-wandb --gpu-id 0 --env-num 1024
 
 # srun -n 1 -N 1 -p gpu python train.py --exp-name "DroneRobust" --act-expert-mode 0 --cri-expert-mode 0 --use-wandb --gpu-id 3
